aa458f63045eb34aada2037fcc224693e123441a
MOH. RAMADAN <69750539+MohRamadan24@users.noreply.github.com>
2024-06-03 11:41:09 +0700

    Update WorkoutForm.js

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -22,7 +22,7 @@ const WorkoutForm = () => {
 
         const workout = {title, load, reps}
 
-        const response = await fetch('/api/workouts', {
+        const response = await fetch('https://trinitydev-1stmernserver.up.railway.app/api/workouts', {
             method: 'POST',
             body: JSON.stringify(workout),
             headers:  {
