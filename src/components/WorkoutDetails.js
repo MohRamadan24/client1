b2b406e36fd857cb881128cc8a5f9c6e436df828
MOH. RAMADAN <69750539+MohRamadan24@users.noreply.github.com>
2024-06-03 11:46:11 +0700

    Update WorkoutDetails.js

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -12,7 +12,7 @@ const WorkoutDetails = ({ workout }) => {
     if(!user) {
       return
     }
-    const response = await  fetch('/api/workouts/' + workout._id, {
+    const response = await  fetch('https://trinitydev-1stmernserver.up.railway.app/api/workouts/' + workout._id, {
       method: 'DELETE',
       headers:  {
         'Authorization': `Bearer ${user.token}`
