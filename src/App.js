43b378b508656da55b0c5102cf27d05f46d52f0a
MOH RAMADAN <69750539+MohRamadan24@users.noreply.github.com>
2024-07-04 20:36:39 +0700

    splash page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthContext } from './hooks/useAuthContext'
 
 // pages & components
+import Splash from './pages/Splash'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
@@ -19,15 +20,19 @@ function App() {
           <Routes>
             <Route 
               path="/" 
+              element={<Splash />} 
+            />
+            <Route 
+              path="/dashboard" 
               element={user ? <Home /> : <Navigate to="/login"/>} 
             />
             <Route 
               path="/login" 
-              element={user ? <Navigate to="/" /> : <Login />} 
+              element={user ? <Navigate to="/dashboard" /> : <Login />} 
             />
             <Route 
               path="/signup" 
-              element={!user ? <Signup /> : <Navigate to="/"/>} 
+              element={!user ? <Signup /> : <Navigate to="/dashboard"/>} 
             />
           </Routes>
         </div>
---
+++ src/pages/Splash.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useAuthContext } from "../hooks/useAuthContext"
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm" 
+import { API_URLS } from "../utils/apiUrls"
+
+const Splash = () => {
+    return (  
+        <div><h1>HEHE</h1></div>
+    );
+}
+ 
+export default Splash;
