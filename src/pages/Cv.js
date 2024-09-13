7244deb964fa141c19db6d36d38974d0506a0226
MOH. RAMADAN <moh.ramadan8h@gmail.com>
2024-09-13 17:02:56 +0700

    Update Cv.js

--- src/pages/Cv.js
+++ src/pages/Cv.js
@@ -1,7 +1,16 @@
 const Cv = () => {
   return (
-    <div>
-        
+    <div className="flex bg-[#f8f8f8] w-full h-auto justify-center items-center py-[100px]">
+      <div className="w-[990px] h-[700px]">
+        <div className="flex flex-row-reverse w-full h-[50px] bg-slate-300 items-center px-[20px]">
+          <div className="w-[35px] h-[35px] bg-slate-500 mx-[15px]"></div>
+          <div className="w-[35px] h-[35px] bg-slate-500 mx-[15px]"></div>
+        </div>
+        <div className="w-[990px] h-[700px] bg-white px-[60px] py-[50px]">
+          <div className="bg-slate-600 w-full h-[200px]"></div>
+          <div className="bg-slate-100 w-full h-[400px]"></div>
+        </div>
+      </div>
     </div>
   )
 }
